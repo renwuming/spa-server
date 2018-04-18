48a0fac2752557b0cc16997c925e7e8ec866e918
renwuming <renwuming@foxmail.com>
2018-04-18 17:44:29 +0800

    feat： 微信支付

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var mid = require('../lib/middleware');
+const weixin = require('../lib/pay');
+const moment = require('moment');
 
 //model
 var Order = require('../Models/order')
@@ -41,19 +43,128 @@ router.get('/find', async function(req, res, next) {
   res.send(result)
 })
 
-//添加订单
-router.post('/', async function(req, res, next) {
-  let sessionid = req.headers.session
-  let appid = await mid.getSessionBy(sessionid)
-  var oneOrder = new Order({...req.body, openId:appid})
+async function submitOrder(data) {
+  let {orderTime, createTime, orderStatus, server, price, name, phoneNumber, address, appid} = data;
+  const _tradeId = 'PM' + moment().format('YYYYMMDDHHmmssSSS');
+  const notify_url = 'https://www.renwuming.cn/maidu/orders/notify';
+  var order = {
+    body: `脉度良子 - ${server}`,
+    attach: server,
+    openid: appid,
+    out_trade_no: _tradeId,
+    total_fee: toCents(data.price),
+    trade_type: 'JSAPI',
+    spbill_create_ip: '127.0.0.1',
+    notify_url,
+  };
+
+  var _order = await weixin.pay.unifiedOrder(order);
+
+  if (_order.return_code == 'SUCCESS' && _order.result_code == 'SUCCESS') {
+    // 订单存入数据库
+    saveOrder(data, appid, _tradeId);
+
+    var jsPay = weixin.pay.toJsPay(_order);
+
+    return {
+      success: true,
+      jsPay: jsPay,
+    };
+  } else {
+    return {
+      success: false,
+      msg: _order.return_code == 'SUCCESS' ? _order.err_code_des : _order.return_msg
+    };
+  }
+}
+
+function saveOrder(data, openId, tradeId) {
+  data.payStatus = false;
+  var oneOrder = new Order({
+    ...data,
+    openId,
+    tradeId,
+  })
   oneOrder.save((err)=>{
     if(err) {
-       res.send(err)
-    } else {
-       res.send('success!')
+      console.log(err);
     }
-  })
+  });
+}
+
+function toCents(n) {
+  return parseInt(n*100);
+}
+
+
+//添加订单
+router.post('/', async function (req, res, next) {
+  let sessionid = req.headers.session
+  let appid = await mid.getSessionBy(sessionid)
+  let data = req.body;
+  let msg;
+  try {
+    data.appid = appid;
+    msg = await submitOrder(data);
+  } catch(e) {
+    console.log(e);
+    res.send({error: e});
+    return;
+  }
+  res.send(msg);
 });
+
+
+
+router.post('/notify', function*() {
+    const data = this.request.body,
+        {xml} = data,
+        {return_code, result_code, sign, out_trade_no} = xml;
+    // xml转json
+    // const xmlParser = new xml2js.Parser({
+    //     trim: true,
+    //     explicitArray: false
+    // });
+    var xmlBuilder = new xml2js.Builder({
+        rootName: 'xml'
+    });
+    // 若成功收到回调消息
+    if (return_code == 'SUCCESS') {
+        delete xml.sign;
+        var _sign = weixin.sign.build(xml);
+        // 若签名校验成功
+        if (sign == _sign) {
+            const status = result_code == 'SUCCESS' ? 1 : 2;
+
+            // 若支付成功
+            if(status===1){
+              // 更新数据库
+              Order.update(
+                { tradeId: out_trade_no },
+                { "$set": {"payStatus": true} }
+              );
+            }
+
+            const result = {
+                return_code: 'SUCCESS',
+                return_msg: 'OK'
+            };
+            const xml = xmlBuilder.buildObject(result);
+            console.log('支付回调返回成功', xml);
+            res.send(xml);
+        } else {
+            // 若签名校验失败
+            const result = {
+                return_code: 'FAIL',
+                return_msg: '签名失败'
+            };
+            const xml = xmlBuilder.buildObject(result);
+            console.log('支付回调返回失败', xml);
+            res.send(xml);
+        }
+    }
+});
+
 
 // 更改订单的状态
 router.put('/', async function(req, res, next) {
