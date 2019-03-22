009ce9a07b8d40789f1796100f62cffd2e4c8536
任无名 <renwuming@foxmail.com>
2019-03-22 11:34:35 +0800

    feat：  1. 用户订单列表，按时间降序排列

--- routes/orders.js
+++ routes/orders.js
@@ -12,50 +12,55 @@ const xmlBuilder = new xml2js.Builder({
 var Order = require('../Models/order')
 var User = require('../Models/usersInfo')
 
-const orderQuery = {payStatus: true, server: {$not: /^recharge$/}};
+const orderQuery = { payStatus: true, server: { $not: /^recharge$/ } };
 
 global.wss.on('connection', function (ws) {
   console.log('client connected')
 
   var sendStockUpdates = async function (ws) {
-      if (ws.readyState == 1) {
-        let result = await Order.find(orderQuery);
-        ws.send(JSON.stringify(result))
-      }
+    if (ws.readyState == 1) {
+      let result = await Order.find(orderQuery);
+      ws.send(JSON.stringify(result))
+    }
   }
 
   var clientStockUpdater = setInterval(function () {
-      sendStockUpdates(ws);
-  }, 1000*30);
+    sendStockUpdates(ws);
+  }, 1000 * 30);
 
 
   ws.on('message', function (message) {
-      console.log(message)
+    console.log(message)
   })
 
 })
 
 // 获取所有的订单
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   let result = await Order.find(orderQuery);
   res.send(result)
 })
 
 // 查询用户订单
-router.get('/find', async function(req, res, next) {
+router.get('/find', async function (req, res, next) {
   let sessionid = req.headers.session
   let appid = await mid.getSessionBy(sessionid)
-  if(!appid) {
-    res.json({errMsg: "sessionkey not found"});
+  if (!appid) {
+    res.json({ errMsg: "sessionkey not found" });
     return;
   }
   //orderQuery.openId = appid;
-  let result = await Order.find({...orderQuery, openId: appid})
+  let result = await Order.find({ ...orderQuery, openId: appid })
+  result.sort((a, b) => {
+    const aTime = +a.tradeId.replace('PM', ''),
+      bTime = +b.tradeId.replace('PM', '')
+    return bTime - aTime
+  })
   res.send(result)
 })
 
 async function submitOrder(data) {
-  let {orderTime, createTime, orderStatus, server, price, name, phoneNumber, address, appid, tradeId} = data;
+  let { orderTime, createTime, orderStatus, server, price, name, phoneNumber, address, appid, tradeId } = data;
   const notify_url = 'https://www.renwuming.cn/maidu/orders/notify';
   var order = {
     body: `脉度良子 - ${server}`,
@@ -94,19 +99,19 @@ function saveOrder(data, payStatus) {
     ...data,
     openId: data.appid,
   })
-  oneOrder.save((err)=>{
-    if(err) {
+  oneOrder.save((err) => {
+    if (err) {
       console.log(err);
     }
   });
 }
 
 function toCents(n) {
-  return parseInt(n*100);
+  return parseInt(n * 100);
 }
 
 function toYuan(n) {
-  return parseFloat(n/100);
+  return parseFloat(n / 100);
 }
 
 
@@ -114,18 +119,18 @@ function toYuan(n) {
 router.post('/', async function (req, res, next) {
   let sessionid = req.headers.session
   let appid = await mid.getSessionBy(sessionid)
-  if(!appid) {
-    res.json({errMsg: "sessionkey not found"});
+  if (!appid) {
+    res.json({ errMsg: "sessionkey not found" });
     return;
   }
   let msg;
   try {
     const tradeId = 'PM' + moment().format('YYYYMMDDHHmmssSSS');
-    let data = {...req.body, tradeId, appid},
-        userInfo = await User.findOne({appid}),
-        {price} = data;
-    if(userInfo&&userInfo.money >= price && req.body.server != 'recharge') { // 非充值订单，使用余额支付
-      await User.update({ appid }, { "$inc": {money: -price } });
+    let data = { ...req.body, tradeId, appid },
+      userInfo = await User.findOne({ appid }),
+      { price } = data;
+    if (userInfo && userInfo.money >= price && req.body.server != 'recharge') { // 非充值订单，使用余额支付
+      await User.update({ appid }, { "$inc": { money: -price } });
       saveOrder(data, true);
       msg = {
         success: true,
@@ -134,9 +139,9 @@ router.post('/', async function (req, res, next) {
     } else { // 调起支付
       msg = await submitOrder(data);
     }
-  } catch(e) {
+  } catch (e) {
     console.log(e);
-    res.send({error: e});
+    res.send({ error: e });
     return;
   }
   res.send(msg);
@@ -144,59 +149,59 @@ router.post('/', async function (req, res, next) {
 
 // 充3000送200
 function handleRecharge(value) {
-  if(value >= 3000 ) return value + 200;
+  if (value >= 3000) return value + 200;
   else return value;
 }
 
 
 router.post('/notify', async function (req, res, next) {
-    const data = req.body,
-        {xml} = data;
+  const data = req.body,
+    { xml } = data;
 
-    // 若成功收到回调消息
-    if (xml && xml.return_code == 'SUCCESS') {
-      let {result_code, sign, out_trade_no, attach, openid, total_fee} = xml;
-        delete xml.sign;
-        var _sign = weixin.sign.build(xml);
-        // 若签名校验成功
-        if (sign == _sign) {
-            // 若支付成功
-            if(result_code == 'SUCCESS'){
-              // 更新数据库
-              try {
-                await Order.update({ tradeId: out_trade_no[0] }, { payStatus: true });
-                if(attach == 'recharge') {
-                  const value = handleRecharge(toYuan(total_fee));
-                  await User.update({ appid: openid }, { "$inc": {money: value } });
-                }
-              } catch(e) {
-                console.log(e);
-              }
-            }
-
-            const result = {
-                return_code: 'SUCCESS',
-                return_msg: 'OK'
-            };
-            const xml = xmlBuilder.buildObject(result);
-            console.log('支付回调返回成功');
-            res.send(xml);
-        } else {
-            // 若签名校验失败
-            const result = {
-                return_code: 'FAIL',
-                return_msg: '签名失败'
-            };
-            const xml = xmlBuilder.buildObject(result);
-            console.log('支付回调返回失败');
-            res.send(xml);
+  // 若成功收到回调消息
+  if (xml && xml.return_code == 'SUCCESS') {
+    let { result_code, sign, out_trade_no, attach, openid, total_fee } = xml;
+    delete xml.sign;
+    var _sign = weixin.sign.build(xml);
+    // 若签名校验成功
+    if (sign == _sign) {
+      // 若支付成功
+      if (result_code == 'SUCCESS') {
+        // 更新数据库
+        try {
+          await Order.update({ tradeId: out_trade_no[0] }, { payStatus: true });
+          if (attach == 'recharge') {
+            const value = handleRecharge(toYuan(total_fee));
+            await User.update({ appid: openid }, { "$inc": { money: value } });
+          }
+        } catch (e) {
+          console.log(e);
         }
+      }
+
+      const result = {
+        return_code: 'SUCCESS',
+        return_msg: 'OK'
+      };
+      const xml = xmlBuilder.buildObject(result);
+      console.log('支付回调返回成功');
+      res.send(xml);
+    } else {
+      // 若签名校验失败
+      const result = {
+        return_code: 'FAIL',
+        return_msg: '签名失败'
+      };
+      const xml = xmlBuilder.buildObject(result);
+      console.log('支付回调返回失败');
+      res.send(xml);
     }
+  }
 });
 
 
 // 更改订单的状态
-router.put('/', async function(req, res, next) {
+router.put('/', async function (req, res, next) {
   let resdata = await Order.update(req.body, { orderStatus: true })
   res.send(resdata)
 })
